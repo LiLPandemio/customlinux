076b1aa774253cef516f2519bf55bc6d99ce0cf8
LiLPandemio <69552375+LiLPandemio@users.noreply.github.com>
2021-11-21 22:50:27 +0100

    Update autoinstall_pt1.sh

--- autoinstall_pt1.sh
+++ autoinstall_pt1.sh
@@ -16,6 +16,8 @@ cd nerd-fonts/
 cd ..
 echo "INSTALANDO EL GESTOR DE PLUGINS DE LA ZSH"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Instalando plugin PL10K"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 echo "Haciendo ZSH tu terminal por defecto, por si el script anterior falla. DESPUES DE EJECUTAR ESTE COMANDO SE MATARA LA SESION ACTUAL Y TENDRAS QUE INICIAR SESION DE NUEVO PARA QUE LOS CAMBIOS SE APLIQUEN. LUEGO EJECUTA EL autoinstall_pt2.sh"
 chsh -s $(which zsh)
 kill -9 -1
